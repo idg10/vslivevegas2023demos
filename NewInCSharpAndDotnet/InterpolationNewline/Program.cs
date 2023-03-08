afe8d1e4ac33f70e4af2e67f168b9aecac8fd621
Ian Griffiths <Ian.Griffiths@endjin.com>
2023-03-08 15:59:24 +0000

    Add newline in string interpolation

---
+++ NewInCSharpAndDotnet/InterpolationNewline/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+void ShowOld(string individual)
+{
+    Console.WriteLine($"{individual} {individual switch { "I" or "You" or "They" => "have", _ => "has", }} {individual switch { "I" => "given a confidential security briefing", "You" => "leaked", _ => "been charged under section 2a of the Official Secrets Act"}}.");
+}
+
+void ShowNew(string individual)
+{
+    Console.WriteLine($"{individual} {individual switch
+    {
+        "I" or "You" or "They" => "have",
+        _ => "has",
+    }} {individual switch
+    {
+        "I" => "given a confidential security briefing",
+        "You" => "leaked",
+        _ => "been charged under section 2a of the Official Secrets Act"
+    }}.");
+}
+
+
+ShowOld("I");
+ShowNew("You");
+ShowNew("She");
