bcc4793d62e807da2e2038db3dcfaf5951bbc6ae
Ian Griffiths <Ian.Griffiths@endjin.com>
2023-03-13 14:47:59 +0000

    Add raw string literal examples

---
+++ NewInCSharpAndDotnet/RawStringLiterals/Program.cs
@@ -0,0 +1,17 @@
+﻿
+string j1 = "{}";
+
+Console.WriteLine(j1);
+
+
+string j2 = "{}";
+
+Console.WriteLine(j2);
+
+
+public static class Example
+{
+    public static void UseJson()
+    {
+    }
+}
---
+++ NewInCSharpAndDotnet/RawStringLiteralsAfter/Program.cs
@@ -0,0 +1,24 @@
+﻿string j1 = """{"myProp":  42}""";
+
+Console.WriteLine(j1);
+
+string gettingSillyNow = """"This contains """ just to make life hard"""";
+
+Example.UseJson(42);
+
+public static class Example
+{
+    public static void UseJson(int value)
+    {
+        string j2 = $$"""
+            {
+              "prop1": {{value}},
+              "prop2": {
+                "nested": [1, 2, 3]
+              }
+            }
+            """;
+
+        Console.WriteLine(j2);
+    }
+}
