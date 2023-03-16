1bd46cb32161f6371874dfe146fd37c92eaca6d6
Ian Griffiths <Ian.Griffiths@endjin.com>
2023-03-16 06:44:39 +0000

    Add primary constructor examples

---
+++ NewInCSharpAndDotnet/PrimaryConstructors/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine(new TypeWithCtor(42, "The answer"));
+
+public class TypeWithCtor
+{
+    private int id;
+    private string displayName;
+
+    public TypeWithCtor(int id, string displayName)
+    {
+        this.id = id;
+        this.displayName = displayName;
+    }
+
+    public override string ToString() => $"{id}: '{displayName}'";
+}
---
+++ NewInCSharpAndDotnet/PrimaryConstructorsAfter/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine(new WithPrimaryConstructor(42, "The answer"));
+
+public class WithoutPrimaryConstructors
+{
+    private int id;
+    private string displayName;
+
+    public WithoutPrimaryConstructors(int id, string displayName)
+    {
+        this.id = id;
+        this.displayName = displayName;
+    }
+
+    public override string ToString() => $"{id}: '{displayName}'";
+}
+
+public class WithPrimaryConstructor(int id, string displayName)
+{
+    public override string ToString() => $"{id}: '{displayName}'";
+}
